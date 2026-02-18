503020199ad85189c10ad7ed39a5bc11e69c4225
lkessels <l@lmpkessels.com>
2026-02-18 16:07:56 +0100

    feat: test for binary search found and not found

---
+++ c/tests/test_binary_search.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <assert.h>
+#include "../algorithms/binary_search.h"
+
+void test_binary_search_found() {
+    int array[5] = {
+        1, 2, 3, 4, 5
+    };
+
+    int size = sizeof(array) / sizeof(array[0]);
+
+    int index = binary_search(array, size, 4);
+
+    assert(index == 4);
+}
+
+void test_binary_search_not_found() {
+    int array[5] = {
+        0, 1, 2, 3, 4
+    };
+
+    int size = sizeof(array) / sizeof(array[0]);
+
+    int index = binary_search(array, size, 5);
+
+    assert(index == -1);
+}
+
+int main(void) {
+    test_binary_search_found();
+    test_binary_search_not_found();
+}
