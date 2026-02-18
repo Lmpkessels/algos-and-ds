e94e0b7e13632dc807d6b124cf5eee33099281ad
lkessels <l@lmpkessels.com>
2026-02-18 16:07:56 +0100

    feat: binary search to find target in array

---
+++ c/algorithms/binary_search.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int binary_search(int array[], int size, int target) {
+    int beginning = 0;
+    int end = size - 1;
+
+    while (beginning <= end) {
+        int middle = beginning + (end - beginning);
+
+        if (array[middle] == target) {
+            return array[middle];
+        };
+
+        if (array[middle] < target) {
+            beginning = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+
+    return -1;
+}
