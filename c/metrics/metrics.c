9239cb6ac2f1bce420265e0fc77c3b859de459c7
lkessels <l@lmpkessels.com>
2026-02-23 12:05:59 +0100

    update: organised directory for metrics

---
+++ c/metrics/metrics.c
@@ -0,0 +1,27 @@
+#include "metrics.h"
+
+metrics_t METRICS = {0};
+
+// Set all metrics back to default.
+void metrics_reset(void) {
+    METRICS.comparisons = 0;
+    METRICS.swaps = 0;
+    METRICS.allocations = 0;
+    METRICS.recursion_depth = 0;
+    METRICS.max_recursion_depth = 0;
+}
+
+// Track recursion depth till max.
+void metrics_enter_recursion(void) {
+    METRICS.recursion_depth++;
+    if (METRICS.recursion_depth > METRICS.max_recursion_depth) {
+        METRICS.max_recursion_depth = METRICS.recursion_depth;
+    }
+}
+
+// End tracking recursion depth.
+void metrics_exit_recursion(void) {
+    if (METRICS.recursion_depth > 0) {
+        METRICS.recursion_depth--;
+    }
+}
