01a76e64bc4db44d066548d2571f3eaf0f6a8b86
lkessels <l@lmpkessels.com>
2026-03-01 13:31:43 +0100

    feat(padding and parsing): little and big both 32 and 64-bit

---
+++ rust/crypto/padd_pars/src/lib.rs
@@ -0,0 +1,15 @@
+pub mod big_endian_padding;
+pub mod big_endian_parsing;
+pub mod big_endian_padding64;
+pub mod big_endian_parsing64;
+pub mod little_endian_padding;
+pub mod little_endian_parsing;
+pub mod keccak_padding;
+
+pub use big_endian_padding::big_endian_padd;
+pub use big_endian_parsing::big_endian_pars;
+pub use big_endian_padding64::big_endian_padd64;
+pub use big_endian_parsing64::big_endian_pars64;
+pub use little_endian_padding::little_endian_padd;
+pub use little_endian_parsing::little_endian_pars;
+pub use keccak_padding::keccak_padd;
